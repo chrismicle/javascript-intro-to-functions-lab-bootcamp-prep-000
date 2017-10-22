28e1366832bd6fbdd570ad46a43084f227b02058
Christina Collura <cmcollura@gmail.com>
2017-10-22 18:30:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(){
-  console.log("Hello!").toUpperCase
+function logShout(string){
+  console.log(`${string}`).toUpperCase
 }
