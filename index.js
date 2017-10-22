a8cf6d8e7e127b741d05c5f5cf8a28eff4714e98
Christina Collura <cmcollura@gmail.com>
2017-10-22 19:32:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   var lowercase = string
   var uppercase = string
-  if (string.toLowerCase() === lowercase){
+  if (string.toLowerCase() === string){
     return "I can't hear you!";
-  } else if (string.toUpperCase() === uppercase) {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
   } else {
     return "I love you, too."
